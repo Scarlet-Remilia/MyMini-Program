778ccc9468dccf6b1051f03f26f89461f46d9439
Scarlet-Remilia <59766610+Scarlet-Remilia@users.noreply.github.com>
2021-04-13 13:38:12 +0800

    登陆状态缓存

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,92 @@
 // index.js
-// 获取应用实例
-const app = getApp()
 Page({
+
+    /**
+     * 页面的初始数据
+     */
     data: {
-        motto: '用户密码登录',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        canIUseGetUserProfile: false,
-        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+
     },
-    // 事件处理函数
-    bindViewTap() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+
+    getToken: function() {
+        // wx.setBackgroundFetchToken({
+        //   token: 'token',
+        // })
+        // wx.getBackgroundFetchToken({
+        //     success: (res) => {},
+        //     fail: (res) => {
+        //         console.log("1");
+        //         // wx.reLaunch({
+        //         //     url: '../../pages/login-page/login-page'
+        //         // })
+        //     },
+        //     complete: (res) => {
+        //         console.log("2");
+        //     }
+        // })
     },
-    onLoad() {
-        if (wx.getUserProfile) {
-            this.setData({
-                canIUseGetUserProfile: true
-            })
-        }
-    },
-    getUserProfile(e) {
-        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-        wx.getUserProfile({
-            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: (res) => {
-                console.log(res)
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        wx.getStorage({
+            key: 'token',
+            fail: (res) => {
+                console.log("未检测到缓存");
+                wx.reLaunch({
+                    url: '../../pages/login-page/login-page'
                 })
-            }
+            },
+            success: (result) => {
+                console.log("每次检测返回");
+            },
         })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {},
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
     }
 })
